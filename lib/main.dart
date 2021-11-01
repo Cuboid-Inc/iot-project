dc9915bcbb98f3ea5e1a0c7fb8d9414e57a8d258
muhammad hassan <hassankhan5805@gmail.com>
2021-11-02 04:29:43 +0500

    gauge meter added

--- lib/main.dart
+++ lib/main.dart
@@ -42,9 +42,22 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             SfRadialGauge(
               axes: <RadialAxis>[
-                RadialAxis(
-                  
-                )
+                RadialAxis(minimum: 0, maximum: 150, ranges: <GaugeRange>[
+                  GaugeRange(startValue: 0, endValue: 50, color: Colors.green),
+                  GaugeRange(
+                      startValue: 50, endValue: 100, color: Colors.orange),
+                  GaugeRange(startValue: 100, endValue: 150, color: Colors.red)
+                ], pointers: <GaugePointer>[
+                  NeedlePointer(value: 90)
+                ], annotations: <GaugeAnnotation>[
+                  GaugeAnnotation(
+                      widget: Container(
+                          child: Text('90.0',
+                              style: TextStyle(
+                                  fontSize: 25, fontWeight: FontWeight.bold))),
+                      angle: 90,
+                      positionFactor: 0.5)
+                ])
               ],
             ),
           ],
